ea7c5a3e69f6554cea8e2289396ef9c48ecd6bb5
Đạt Nguyễn <53225576+tiendat21896@users.noreply.github.com>
2020-08-17 11:34:09 +0700

    17/8/2020

---
+++ Observable Collection/Observable Collection/Models/Contact.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observable_Collection.Models
+{
+    class Contact
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string AvatarPath { get; set; }
+    }
+}
---
+++ Observable Collection/Observable Collection/Models/Icon.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observable_Collection.Models
+{
+    class Icon
+    {
+        public string IconPath { get; set; }
+    }
+}
