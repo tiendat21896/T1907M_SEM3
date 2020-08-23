beb020864c70495a116d8f68d65c074c4b079246
Đạt Nguyễn <53225576+tiendat21896@users.noreply.github.com>
2020-08-23 23:03:47 +0700

    new

---
+++ YoutubeAPI/MainPage.xaml.cs
@@ -0,0 +1,95 @@
+﻿using Google.Apis.Services;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Popups;
+using System.Net.NetworkInformation;
+using Google.Apis.YouTube.v3.Data;
+using Google.Apis.YouTube.v3;
+using Video = YoutubeAPI.Model.Video;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace YoutubeAPI
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private YouTubeService youTubeService = 
+            new YouTubeService(new BaseClientService.Initializer 
+            { 
+            ApiKey = "AIzaSyDBf8bq5AKUSHfF_CF0eeZ2RCLzyfmOi5s",
+            ApplicationName = "youtube"
+            });
+        List<Video> ListVideo = new List<Video>();
+        private string TokenNextpage = null, TokenPrivPage = null;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            GetVideo();
+        }
+
+        private async void GetVideo(string PageToken = null)
+        {
+            try
+            {
+                if(NetworkInterface.GetIsNetworkAvailable())
+                {
+                    var Request = youTubeService.Search.List("snippet");
+                    Request.ChannelId = "UCsooa4yRKGN_zEE8iknghZA";
+                    Request.MaxResults = 25;
+                    Request.Type = "video";
+                    Request.Order = SearchResource.ListRequest.OrderEnum.Date;
+                    Request.PageToken = PageToken;
+                    var Result = await Request.ExecuteAsync();
+                    if (Result.NextPageToken != null)
+                        TokenNextpage = Result.NextPageToken;
+                    if (Result.PrevPageToken != null)
+                        TokenPrivPage = Result.PrevPageToken;
+
+                    foreach (var item in Result.Items)
+                    {
+                        ListVideo.Add(new Model.Video
+                        {
+                            Title = item.Snippet.Title,
+                            Id = item.Id.VideoId,
+                            Img = item.Snippet.Thumbnails.Default__.Url
+                            
+                        });
+                    }
+                    lv.ItemsSource = null;
+                    lv.ItemsSource = ListVideo;
+                }
+                else
+                {
+                    MessageDialog msg = new MessageDialog("Check your internet connection");
+                    await msg.ShowAsync();
+                }
+            }
+            catch
+            {
+
+            }
+        }
+        private void lv_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            Video video = e.ClickedItem as Video;
+            Frame.Navigate(typeof(VideoPage), video);
+
+        }
+    }
+}
---
+++ YoutubeAPI/Model/Video.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YoutubeAPI.Model
+{
+    class Video
+    {
+        public string Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Img { get; set; }
+
+        public string Descriptiong { get; set; }
+
+        public DateTime? PubDate { get; set; }
+    }
+}
